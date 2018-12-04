f21b268a63361cf036e955854e149a62528573c3
ersaurav03 <er.saurav03@gmail.com>
2018-12-04 16:59:01 +0530

    initial commit

---
+++ November work/work after 27/js.js
@@ -0,0 +1,28 @@
+function sayhello()
+{
+document.write("JSMPJ Corporation");
+}
+function fn(age, name)
+{
+document.write("age is "+age+"name is"+name);
+}
+function myFun()
+{
+document.getElementById("demo4").innerHTML="This is JSMPJ Corporation";
+}
+
+
+	/*alert("JSMPJ Corporation");
+    document.write("we are on the page");
+    console.log("we are at console");
+
+
+var live=3;
+var example=" i am a variable";
+document.write(example);
+document.write("variable is "+live);
+
+var x=prompt(" what is your age ");
+var y=prompt(" what is your name ");
+document.write("hello"+y+"today your age is"+x);*/
+
---
+++ November work/worktill27/html work/html/all/putss.rb
@@ -0,0 +1,2 @@
+puts "Maropost will be worlds largest It company in 2025."
+puts "I have done this."
